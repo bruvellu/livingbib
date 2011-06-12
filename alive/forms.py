f0fc81ba23d9f3b296c72f53c0f07c5d21410fa2
Bruno Vellutini <organelas@gmail.com>
2011-06-12 04:53:19 -0300

    Adds missing forms file

---
+++ alive/forms.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+ORDER_BY = (
+        ('stats__readers', _('readers')),
+        ('title', _('title')),
+        ('year', _('year')),
+        ('rank', _('rank')),
+        ('type', _('type')),
+        ('publication_outlet', _('journal')),
+        ('publisher', _('publisher')),
+        )
+
+class SortForm(forms.Form):
+    type = forms.ChoiceField(choices=ORDER_BY, label=_('Sort by'))
+    reverse = forms.BooleanField(required=False, initial=False, label=_('Reverse'))
