7a17a8822e35379e84b7e9fa3ab20f2114031521
Bruno Vellutini <organelas@gmail.com>
2011-06-25 16:49:21 -0300

    Notice to fix doubled queries

--- alive/commands.py
+++ alive/commands.py
@@ -4,4 +4,5 @@ from livingbib.runalive import fetch
 @queue_command
 def fetch_references(taxon_name):
     '''Fetch references from Mendeley related to a taxon.'''
+    #FIXME Check if query is already in queue line before adding.
     fetch(taxon_name)
