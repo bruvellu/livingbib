ccc03419cdf3fad877377cccf66a99213cbf915c
Bruno Vellutini <organelas@gmail.com>
2011-09-26 15:49:47 -0300

    Set the basic elements for base and home templates

--- alive/forms.py
+++ alive/forms.py
@@ -13,6 +13,7 @@ ORDER_BY = (
         ('publisher', _('publisher')),
         )
 
+
 class SortForm(forms.Form):
     type = forms.ChoiceField(choices=ORDER_BY, label=_('Sort by'))
     reverse = forms.BooleanField(required=False, initial=False, label=_('Reverse'))
@@ -21,6 +22,9 @@ class SortForm(forms.Form):
 class SearchForm(forms.Form):
     query = forms.CharField(
             label=_('Search for'),
-            widget=forms.TextInput(attrs={'size': 44}),
+            widget=forms.TextInput(attrs={
+                'class': 'input-large',
+                'placeholder': 'a scientific or common name',
+                }),
             help_text=_('try a scientific or common name'),
             )
